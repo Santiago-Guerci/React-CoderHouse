f2d9d079434c7ce628133e5749a9398856227c02
Santiago Güerci <santiago.38@hotmail.com>
2022-03-29 22:29:54 -0300

    vercel ok

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -12,15 +12,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_AppId
 };
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_ApiKey,
-//   authDomain: process.env.REACT_APP_AuthDomain,
-//   projectId: process.env.REACT_APP_ProjectId,
-//   storageBucket: process.env.REACT_APP_StorageBucket,
-//   messagingSenderId: process.env.REACT_APP_MessagingSenderId,
-//   appId: process.env.REACT_APP_AppId
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
